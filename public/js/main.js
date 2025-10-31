5a19567464a43ac65d0bff20426db1b84f772697
HkTheBeast <marvelousdirectioner@gmail.com>
2025-10-31 20:55:19 +0530

    j

--- public/js/main.js
+++ public/js/main.js
@@ -98,3 +98,20 @@
   // Initial check
   refreshAuthUI();
 })();
+  
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px"; // Set max-height to content's scroll height
+    } 
+  });
+}
