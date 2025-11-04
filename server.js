e38bd083f11121cda98287d6eaf82ab4bc0de1c1
HkTheBeast <marvelousdirectioner@gmail.com>
2025-11-04 05:45:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json({limit: '5mb'}));
